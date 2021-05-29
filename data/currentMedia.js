435b0c4ecae99ef8f33431ab3646e54eebefc622
Maximiliano Falicoff <mfalicoff@yahoo.com>
2021-05-28 23:05:33 -0400

    current games

--- data/currentMedia.js
+++ data/currentMedia.js
@@ -15,6 +15,15 @@ const currentMedia = [
     href: 'https://store.steampowered.com/app/632470/Disco_Elysium__The_Final_Cut/',
   },
   {
+    title: '13 Sentinels: Aegis Rim',
+    description: `Vanillaware, the storytellers behind Odin Sphere and Dragon’s Crown, craft a sci-fi mystery epic spanning thirteen intertwining stories in 13 Sentinels: Aegis Rim. With a Metacritic score of 86, 13 Sentinels has won the praise of critics all over the world.
+
+Uncover the truth and delve into a 2D sidescrolling adventure featuring gorgeous art and environments. Then, battle the kaiju in fast-paced, top-down combat. Customize the Sentinels with an arsenal of mechsuit weaponry, and fight to defend humanity!`,
+    imgSrc: '/static/images/games/13Sentinels.jpg',
+    type: 'Game',
+    href: 'https://store.playstation.com/en-us/product/UP0177-CUSA19610_00-BSTUBCUSA1961000',
+  },
+  {
     title: 'Red Seas Under Red Skies by Scott Lynch',
     description: `After a brutal battle with the underworld that nearly destroyed him, Locke Lamora and his trusted sidekick, Jean, 
     fled the island city of their birth and landed on the exotic shores of Tal Verrar to nurse their wounds. 
